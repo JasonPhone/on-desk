5b0dd537e93d56cf742628843b9323dc96e82546
ja50n <zs_feng@qq.com>
2022-02-11 17:54:25 +0800

    done class FineTitle Note that this class is abstract one, it need to implement doule click event.

--- OnDesk_zh_CN.ts
+++ OnDesk_zh_CN.ts
@@ -2,6 +2,29 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="zh_CN">
 <context>
+    <name>FineTitle</name>
+    <message>
+        <location filename="finetitle.cpp" line="108"/>
+        <source>Minimize</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="finetitle.cpp" line="113"/>
+        <source>Restore</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="finetitle.cpp" line="118"/>
+        <source>Maximize</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="finetitle.cpp" line="123"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>MemoManager</name>
     <message>
         <location filename="memomanager.ui" line="14"/>
